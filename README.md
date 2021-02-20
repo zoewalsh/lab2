51f9e53e800a83168e2310be9705599adb44f3c6
zoewalsh <zoeswalsh@gmail.com>
2021-02-20 11:49:48 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Project 2
+ENGO 551
+
+The purpose of this web application is to 
---
+++ application.py
@@ -0,0 +1,29 @@
+import os
+import sys
+
+from flask import Flask, session, render_template, request, flash, redirect, abort
+from flask_session import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from werkzeug.security import generate_password_hash, check_password_hash
+import requests
+
+app = Flask(__name__)
+
+# Check for environment variable
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
+
+# fix windows terminal issue on my computer
+if sys.platform.lower() == "win64":
+    os.system('color')
+
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
+@app.route("/", methods=['POST', 'GET'])
+def index():
+    
