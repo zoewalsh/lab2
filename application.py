b7e8389c57d8a8f8f13f0d6119d9cacf092d56bf
zoewalsh <zoeswalsh@gmail.com>
2021-02-20 13:41:27 -0700

    update

--- application.py
+++ application.py
@@ -23,3 +23,16 @@ Session(app)
 @app.route("/", methods=['POST', 'GET'])
 def index():
     return render_template("index.html")
+
+# need to remove existing geojson layer every time there is a new query - filter wont apply to existing data
+
+@app.route("/dates", methods=['POST'])
+def dates():
+    # get from and to dates from the date picker
+    fr = request.form.get("from")
+    to = request.form.get("to")
+    url = "https://data.calgary.ca/resource/c2es-76ed.geojson?$where=issueddate > '"+fr+"' and issueddate < '"+to+"'&$select=issueddate,workclassgroup,contractorname,communityname,originaladdress"
+
+    data = requests.get(url)
+    # convert
+    return render_template("index.html")
