41d334194f67edd2fc9fa02cfb91fb30650bbdc8
zoewalsh <zoeswalsh@gmail.com>
2021-02-21 19:10:23 -0700

    points and popups

--- application.py
+++ application.py
@@ -1,12 +1,14 @@
 import os
 import sys
 
-from flask import Flask, session, render_template, request, flash, redirect, abort
+from flask import Flask, session, render_template, request, flash, redirect, abort, jsonify
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from werkzeug.security import generate_password_hash, check_password_hash
 import requests
+import geojson
+import json
 
 app = Flask(__name__)
 
@@ -28,11 +30,12 @@ def index():
 
 @app.route("/dates", methods=['POST'])
 def dates():
+    app_token="Htp39ZMQbN1AmuufzF1ZlgdFt"
     # get from and to dates from the date picker
     fr = request.form.get("from")
     to = request.form.get("to")
-    url = "https://data.calgary.ca/resource/c2es-76ed.geojson?$where=issueddate > '"+fr+"' and issueddate < '"+to+"'&$select=issueddate,workclassgroup,contractorname,communityname,originaladdress"
-
-    data = requests.get(url)
-    # convert
-    return render_template("index.html")
+    url = "https://data.calgary.ca/resource/c2es-76ed.geojson?$where=issueddate > '"+fr+"' and issueddate < '"+to+"'"
+    data = requests.get(url).json()
+    data2 = data['features']
+    print(data2)
+    return render_template("index.html",data=data2)
